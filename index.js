b8ae00c1ffaa9fcf5fd21197ae66aec0714f3d54
grace-mugoiri <gracemugoiri@gmail.com>
2019-10-25 10:54:32 +0300

    fix linter

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const myKey = 'c6ee67c2af79dc6be8e156787df2cbfe';
 const form = document.querySelector('form');
 
-window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-		let dropdowns = document.getElementsByClassName('dropdown-content');
-		let i;
-		for (i = 0; i < dropdowns.length; i++) {
-			let openDropDown = dropdowns[i];
-			if (openDropDown.classList.contains('show')) {
-				openDropDown.classList.remove('show');
-			}
-		}
-	}
-}
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        const dropdowns = document.getElementsByClassName('dropdown-content');
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropDown = dropdowns[i];
+            if (openDropDown.classList.contains('show')) {
+                openDropDown.classList.remove('show');
+            }
+        }
+    }
+};
 
 const displayWeather = (weather, temp) => {
-	document.getElementById('the-weather').innerText = `${Math.round((temp - 273.15) * 1.8) + 32}\u2109 and ${weather}`;
+    document.getElementById('the-weather').innerText = `${Math.round((temp - 273.15) * 1.8) + 32}\u2109 and ${weather}`;
 };
 
 const fetchWeather = (cities) => {
-	let city = cities;
-	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myKey}`, {mode: 'cors'})
+	const city = cities;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=${ myKey }`, { mode: 'cors' })
 	.then(response => response.json())
-	.then((response) => {
-		let weather = response.name;
-		displayWeather(response.weather[0].description, response.main.temp);
-	})
-	.catch(() => {
-		document.getElementById('the-weather').innerText = "THAT IS NOT A VALID CITY. CHOOSE AGAIN PLEASE!";
-	});
+    .then((response) => {
+        let weather = response.name;
+        displayWeather(response.weather[0].description, response.main.temp);
+    })
+    .catch(() => {
+        document.getElementById('the-weather').innerText = "THAT IS NOT A VALID CITY. CHOOSE AGAIN PLEASE!";
+    });
 };
 form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	fetchWeather(form.elements.city.value);
+    event.preventDefault();
+    fetchWeather(form.elements.city.value);
 });
