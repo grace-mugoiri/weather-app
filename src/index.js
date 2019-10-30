2ffa294445bcb70b5919b4829d30abf0286fe4a4
grace-mugoiri <gracemugoiri@gmail.com>
2019-10-30 15:58:18 +0300

    modify js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ window.addEventListener('load', () => {
   let long;
   const myKey = 'c6ee67c2af79dc6be8e156787df2cbfe';
   let city;
-	// const cityRegEx = /(?<city>\w+)/;
 
   const cityName = document.querySelector('#city-name');
   const mainContent = document.getElementById('main-content');
@@ -14,7 +13,37 @@ window.addEventListener('load', () => {
   const tempSection = document.querySelector('.temp-section');
   const tempSectionSpan = document.querySelector('.temp-section span');
 
-  mainContent.classList.remove('rainy', 'cloudy', 'clear');
+	mainContent.classList.remove('rainy', 'cloudy', 'clear');
+
+	function displayWeather(description, temp, main, name) {
+    const celsius = Math.round(temp - 273.15);
+    const fahrenheit = Math.round((temp - 273.15) * 1.8 + 32);
+
+    weatherDesc.textContent = description;
+    weatherInfo.textContent = name;
+    weatherType.textContent = main;
+    currentTemp.textContent = `${celsius}`;
+
+    alert('click degrees to see in °C or °F');
+
+    tempSection.addEventListener('click', () => {
+      if (tempSectionSpan.textContent === '°C') {
+        tempSectionSpan.textContent = '°F';
+        currentTemp.textContent = `${fahrenheit}`;
+      } else {
+        tempSectionSpan.textContent = '°C';
+        currentTemp.textContent = `${celsius}`;
+      }
+    });
+
+    if (description.includes('rain')) {
+      mainContent.classList.add('rainy');
+    } else if (description.includes('cloud')) {
+      mainContent.classList.add('cloudy');
+    } else {
+      mainContent.classList.add('clear');
+    }
+  }
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -48,7 +77,6 @@ window.addEventListener('load', () => {
       fetch(weatherApi, { mode: 'cors' })
         .then(response => response.json())
         .then((data) => {
-          console.log(data);
           const { temp } = data.main;
           const { main, description } = data.weather[0];
           const { name } = data;
@@ -56,40 +84,11 @@ window.addEventListener('load', () => {
           displayWeather(description, temp, name, main);
         })
         .catch((e) => {
-          console.log(e);
+					return e;
         });
     } else {
       alert('Enter a city');
     }
   });
 
-  function displayWeather(description, temp, main, name) {
-    const celsius = Math.round(temp - 273.15);
-    const fahrenheit = Math.round((temp - 273.15) * 1.8 + 32);
-
-    weatherDesc.textContent = description;
-    weatherInfo.textContent = name;
-    weatherType.textContent = main;
-    currentTemp.textContent = `${celsius}`;
-
-    alert('click degrees to see in °C or °F');
-
-    tempSection.addEventListener('click', () => {
-      if (tempSectionSpan.textContent === '°C') {
-        tempSectionSpan.textContent = '°F';
-        currentTemp.textContent = `${fahrenheit}`;
-      } else {
-        tempSectionSpan.textContent = '°C';
-        currentTemp.textContent = `${celsius}`;
-      }
-    });
-
-    if (description.includes('rain')) {
-      mainContent.classList.add('rainy');
-    } else if (description.includes('cloud')) {
-      mainContent.classList.add('cloudy');
-    } else {
-      mainContent.classList.add('clear');
-    }
-  }
 });
