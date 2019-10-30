d6b998cd51bcc357de71ab1c3898c36ab4371605
grace-mugoiri <gracemugoiri@gmail.com>
2019-10-30 11:58:21 +0300

    modify webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+ },
 };
